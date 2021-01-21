6d29cdfb1f786d0fa2392173bca31d4fbbf052b7
Michael Lai <mrlai7782@gmail.com>
2021-01-21 04:28:50 -0800

    Fixed read/delete bugs

--- script.js
+++ script.js
@@ -2,18 +2,54 @@
 
 let myLibrary = [];
 
-let bookA = new Book('bookA', 'authorA', 100, 'yes');
+let bookA = new Book('Z', 'authorA', 100, 'yes');
 myLibrary.push(bookA);
-let bookB = new Book('bookB', 'authorB', 100, 'yes');
+let bookB = new Book('Y', 'authorB', 100, 'yes');
 myLibrary.push(bookB);
-let bookC = new Book('bookC', 'authorC', 100, 'no');
+let bookC = new Book('X', 'authorC', 100, 'no');
 myLibrary.push(bookC);
-let bookD = new Book('bookD', 'authorD', 100, 'yes');
+let bookD = new Book('W', 'authorD', 100, 'yes');
 myLibrary.push(bookD);
 let bookE = new Book('bookE', 'authorE', 100, 'yes');
 myLibrary.push(bookE);
 let bookF = new Book('bookF', 'authorF', 100, 'no');
 myLibrary.push(bookF);
+let bookG = new Book('bookG', 'authorA', 100, 'yes');
+myLibrary.push(bookG);
+let bookH = new Book('bookH', 'authorB', 100, 'yes');
+myLibrary.push(bookH);
+let bookI = new Book('bookI', 'authorC', 100, 'no');
+myLibrary.push(bookI);
+let bookJ = new Book('bookJ', 'authorD', 100, 'yes');
+myLibrary.push(bookJ);
+let bookK = new Book('bookK', 'authorE', 100, 'yes');
+myLibrary.push(bookK);
+let bookL = new Book('bookL', 'authorF', 100, 'no');
+myLibrary.push(bookL);
+let bookM = new Book('bookM', 'authorA', 100, 'yes');
+myLibrary.push(bookM);
+let bookN = new Book('bookN', 'authorB', 100, 'yes');
+myLibrary.push(bookN);
+let bookO = new Book('bookO', 'authorC', 100, 'no');
+myLibrary.push(bookO);
+let bookP = new Book('bookP', 'authorD', 100, 'yes');
+myLibrary.push(bookP);
+let bookQ = new Book('bookQ', 'authorE', 100, 'yes');
+myLibrary.push(bookQ);
+let bookR = new Book('bookR', 'authorF', 100, 'no');
+myLibrary.push(bookR);
+let bookS = new Book('bookS', 'authorA', 100, 'yes');
+myLibrary.push(bookS);
+let bookT = new Book('bookT', 'authorB', 100, 'yes');
+myLibrary.push(bookT);
+let bookU = new Book('bookU', 'authorC', 100, 'no');
+myLibrary.push(bookU);
+let bookV = new Book('A', 'authorD', 100, 'yes');
+myLibrary.push(bookV);
+let bookW = new Book('B', 'authorE', 100, 'yes');
+myLibrary.push(bookW);
+let bookX = new Book('C', 'authorF', 100, 'no');
+myLibrary.push(bookX);
 
 
 function Book(title, author, pages, read) {
@@ -26,7 +62,7 @@ function Book(title, author, pages, read) {
         return title + ' by ' + author + ', ' + pages + ' pages, ' + read;
     }
 
-    this.index = 'card' + myLibrary.length;
+    this.index = title;
 }
 
 function addBookToLibrary() {
@@ -48,7 +84,7 @@ function addBookToLibrary() {
 
 function drawBooks(myLibrary) {
 
-    document.querySelector('.books-container').innerHTML = '';
+    document.querySelector('.cards-container').innerHTML = '';
     let book;
 
     for (let i = 0; i < myLibrary.length; i++) {
@@ -67,31 +103,47 @@ function drawBooks(myLibrary) {
                 </div>
             </div>`;
 
-        document.querySelector('.books-container').prepend(book); 
+        document.querySelector('.cards-container').appendChild(book); 
 
     }
 }
 
 function deleteBook() {
-    let i = Number(this.className.slice(37));
-    myLibrary.splice(i, 1);
+    let i = this.className.slice(33);
+    console.log(i);
+    for (let j = 0; j < myLibrary.length; j++) {
+        if (myLibrary[j].index === i) {
+            myLibrary.splice(j, 1);
+        }
+    }
+
     start();
     console.table(myLibrary);
 }
 
 function readBook() {
-    let i = Number(this.className.slice(35));
-    if (myLibrary[i].read === 'yes') {
-        myLibrary[i].read = 'no';
-    } else {
-        myLibrary[i].read = 'yes';
+    let i = this.className.slice(31);
+    console.log(i);
+    for (let j = 0; j < myLibrary.length; j++) {
+        if (myLibrary[j].index === i) {
+            if (myLibrary[j].read === 'yes') myLibrary[j].read = 'no';
+            else myLibrary[j].read = 'yes';
+        }
     }
-
     start();
     console.table(myLibrary); 
 }
 
 function start() {
+
+    myLibrary.sort(function(a, b) {
+        if (a.title < b.title) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
     drawBooks(myLibrary);
 
     const newBookButton = document.querySelector('.new-book-button');
